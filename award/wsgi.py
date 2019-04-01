e393209f88d637e2dbfdee708e36f2d47d427518
munganyendesandrine <mungasandwingsd@gmail.com>
2019-04-01 11:40:15 +0200

    new updates

--- award/wsgi.py
+++ award/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Award project.
+WSGI config for award project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Award.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "award.settings")
 
 application = get_wsgi_application()
---
+++ award_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AwardAppConfig(AppConfig):
+    name = 'award_app'
