33708c7956c27a7aef792163d904b63a0eaaced6
munganyendesandrine <mungasandwingsd@gmail.com>
2019-04-03 12:35:00 +0200

    updates

--- award/settings.py
+++ award/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-LOGIN_REDIRECT_URL = 'myprofile'
+LOGIN_REDIRECT_URL = 'myprofilepage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- award_app/urls.py
+++ award_app/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 urlpatterns=[
 
     url(r'^$',views.welcome, name='welcome'),
-    url(r'^new/profileform$', views.my_profile, name='myprofile'),
     url(r'^new/profilepage$', views.profile_page, name='myprofilepage'),
+    url(r'^new/profileform$', views.my_profile, name='myprofile'),
     url(r'^new/project_image$', views.my_picture, name='project_image'),
     url(r'^new/project_image$', views.search_results, name='search_results'),
 ]
