af4ba63e6de90c06065d73aba7de08c56d07feb0
munganyendesandrine <mungasandwingsd@gmail.com>
2019-04-04 11:38:15 +0200

    API endpoint for Profile model

--- award_app/views.py
+++ award_app/views.py
@@ -8,20 +8,23 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import ProfileSerializer
 from rest_framework import status
+from .permissions import IsAdminOrReadOnly
 
-#........
+
 class ProfileList(APIView):
+    
     def get(self, request, format=None):
         all_merch = Profile.objects.all()
         serializers = ProfileSerializer(all_merch, many=True)
         return Response(serializers.data)
-
+        
     def post(self, request, format=None):
         serializers = ProfileSerializer(data=request.data)
         if serializers.is_valid():
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+        permission_classes = (IsAdminOrReadOnly)
 
 # Create your views here.
 
